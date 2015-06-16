db2c40824b36e28c4995068da0dd0060c1abec81
Kevkev1 <kevin1226@live.nl>
2015-06-16 12:33:43 +0200

    There be a tokenizer now

--- grammar.hs
+++ grammar.hs
@@ -56,16 +56,18 @@ tokenizer :: State -> String -> String -> [Token]
 tokenizer _ [] word = []
 tokenizer ERROR _ _ = error "Shiver me timbers! You done it wrong."
 tokenizer START (x:xs) _    
-	| length (checkKeywords [x]) > 1 = tokenizer KW (x:xs) ""
+	| length (checkKeywords [x]) >= 1 = tokenizer KW (x:xs) ""
 	| otherwise = tokenizer ERROR xs ""
 tokenizer KW (x:xs) word    
-	| length possibleKeywords > 1 = tokenizer KW xs newWord
+	| length possibleKeywords > 1 						= tokenizer KW xs newWord
 	| length possibleKeywords == 1 && startsWith " " xs = (Keyword newWord, newWord): tokenizer START (tail xs) ""
-	| length possibleKeywords == 0 = tokenizer ERROR xs word
-	| otherwise = tokenizer ERROR xs word
+	| length possibleKeywords == 1 && xs == [] 			= [(Keyword newWord, newWord)]
+	| length possibleKeywords == 1 						= tokenizer KW xs newWord
+	| length possibleKeywords == 0 						= tokenizer ERROR xs word
+	| otherwise 										= tokenizer ERROR xs word
 	where 
 		possibleKeywords = checkKeywords newWord
-		newWord = (x:word)
+		newWord = word ++ [x]
 
 checkKeywords ::  String -> [String]
 checkKeywords possibleToken = [x | x <- allKeywords, startsWith possibleToken x]
