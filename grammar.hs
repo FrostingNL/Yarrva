33296eb8f60c7a8a728dd7539c71713853c102fc
Kevkev1 <kevin1226@live.nl>
2015-06-16 09:49:08 +0200

    fsd

--- grammar.hs
+++ grammar.hs
@@ -1,5 +1,12 @@
 import Parse
 
+{-
+TO-DO:
+	- Tokenizer
+	- Testing (/w GUI)
+	- Do the rest
+-}
+
 grammar :: Grammar
 grammar nt = case nt of
 	Program -> [[progKey, lcbr, Rep0 [Expr], rcbr]]
@@ -16,7 +23,6 @@ trueKey 	= Keyword "Aye"
 falseKey 	= Keyword "Nay"
 varKey 		= Keyword "booty"
 ifExprKey	= Keyword "parley"
-whileKey	= Keyword "while"
 elseifKey	= Keyword "heave to"
 elseKey 	= Keyword "heave ho"
 breakKey	= Keyword "belay"
@@ -44,38 +50,38 @@ colon   = Symbol ":"
 star	= Symbol "*"
 
 sampleProgram = concat ["fleet Sample {",
-						"	booty a be 3!",
-						"	booty b be 6!",
-						"	booty c be a+b!",
-						"	booty d be Aye!",
-						"	parlay(d) { *: This is a comment Arr.",
-						"		parrot c!",
-						" 	}",
-						"	heave ho {",
-						"		parrot Nay!",
-						"	}",
-						"	whirlpool(d) {",
-						"		parlay(c be 10) {",
-						"			belay!",
-						"		}",
-						"		c be c + 1!",
-						"	}",
+						"   booty a be 3!",
+						"   booty b be 6!",
+						"   booty c be a+b!",
+						"   booty d be Aye!",
+						"   parlay(d) { *: This is a comment Arr.",
+						"      parrot c!",
+						"   }",
+						"   heave ho {",
+						"      parrot Nay!",
+						"   }",
+						"   whirlpool(d) {",
+						"      parlay(c be 10) {",
+						"         belay!",
+						"      }",
+						"      c be c + 1!",
+						"   }",
 						"}"						
 						]
 
 helloWorld = concat ["fleet HelloWorld {",
-					 "	parrot \"Ahoy World!\" !",
+					 "   parrot \"Ahoy World!\" !",
 					 "}"
 					]
 
-sampleFunction = concat ["fleet SampleFunction {"
-						 "	ship add3(booty i) {",
-						 "		avast i + 3!",
-						 "	}",
-						 "	",
-						 "	flagship() {",
-						 "		booty a be add3(5)!",
-						 "		parrot a!",
-						 "	}",
+sampleFunction = concat ["fleet SampleFunction {",
+						 "   ship add3(booty i) {",
+						 "      avast i + 3!",
+						 "   }",
+						 "   ",
+						 "   flagship() {",
+						 "      booty a be add3(5)!",
+						 "      parrot a!",
+						 "   }",
 						 "}"
 						]
