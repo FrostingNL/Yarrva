5bbf312f3b748819d485ba07e820cf36d8b47d6a
Kevkev1 <kevin1226@live.nl>
2015-06-18 17:42:33 +0200

    Checker

--- checker.hs
+++ checker.hs
@@ -3,6 +3,37 @@ import FPPrac.Trees
 import Data.List
 import Grammar
 
+typeChecker :: [[(String, Types)]] -> Tree -> Bool
+typeChecker list (ZupaNode s xs) = all (==True) (map (typeChecker newList) allNodes)
+								 where
+								 	newList 	= (addToScope xs): list
+								 	allNodes 	= getOtherNodes xs
+
+getOtherNodes :: [Tree] -> [Tree]
+getOtherNodes []						= []
+getOtherNodes ((BootyNode _ _): xs) 	= getOtherNodes xs
+getOtherNodes ((DoubloonNode _ _): xs)	= getOtherNodes xs
+getOtherNodes ((BoolNode _ _): xs)		= getOtherNodes xs
+getOtherNodes (n:xs)					= n: getOtherNodes xs
+
+addToScope :: [Tree] -> [(String, Types)]
+addToScope [] 									= []
+addToScope ((BootyNode (VarNode s) t): xs)		| checkType t Str 	= (s, Str): addToScope xs 
+												| otherwise 		= (s, Err): addToScope xs
+addToScope ((DoubloonNode (VarNode s) t): xs)	| checkType t Int 	= (s, Int): addToScope xs 
+												| otherwise 		= (s, Err): addToScope xs
+addToScope ((BoolNode (VarNode s) t): xs)		| checkType t Boo 	= (s, Boo): addToScope xs 
+												| otherwise 		= (s, Err): addToScope xs
+addToScope (_: xs)								= addToScope xs
+
+checkType :: Tree -> Types -> Bool
+checkType (VarNode s) Int 				= all (==True) (map (isNumber) s)
+checkType (VarNode s) Str 				= all (==True) (map (isNumber) s)
+checkType (VarNode s) Boo 				= all (==True) (map (isNumber) s)
+checkType (OpNode s t1 t2) Int			= checkType t1 Int && checkType t2 Int
+checkType (OpNode "+" t1 t2) Str 		= checkType t1 Str && checkType t2 Str
+checkType _ _ 							= False
+
 getType :: String -> [[(String, Types)]] -> Types
 getType	s []					= Err
 getType s ([]:x:list)			= getType s (x:list)
